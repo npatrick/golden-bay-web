55c3284dcc13ffabedd8f1cbb08d020e90cccac7
Nicko Romana <wazzupnickz@gmail.com>
2017-03-23 19:01:31 -0700

    add home component

--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Navigation from './navigation';
+import Home from './home';
 
 export default class App extends Component {
  render() {
 		return (
 			<div>
 				<Navigation />
+				<Home />
 			</div>
 		)
 	}
---
+++ src/client/components/home.js
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Home = () => {
+		return (
+			<header id="header">
+	      <div className="container">
+          <div className="row">
+            <div className="col-xs-10 col-xs-offset-1 col-sm-5" id="intro">
+              <h1><i className="fa fa-sun-o logo-circled"></i> <strong>Golden Bay</strong>Health & Staffing Services</h1>
+              <br />
+
+              <div className="ticker">
+                <h3><strong>Quality</strong> care.</h3>
+                <h3><strong>Committed</strong> staff.</h3>
+                <h3><strong>Preserving</strong> independence.</h3>
+              </div>
+
+              <p>Affordable solution for non-medical quality In-Home Care.</p>
+              <br />
+              <a href="#about" className="btn btn-transparent scroll"><strong>LEARN MORE</strong></a>
+              <a href="#about" className="btn btn-link btn-white scroll">See Our Rates <i className="fa fa-angle-right"></i></a>
+            </div>
+          </div>
+	      </div>
+      </header>
+		)
+	
+}
+
+export default Home;
