a6102a0d80ddf2fc4513a304c9052fe87303cffc
Nicko Romana <wazzupnickz@gmail.com>
2017-04-04 23:00:06 -0700

    add footer component

---
+++ src/client/components/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+
+const Footer = () => {
+	return (
+		<footer id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-xs-12">
+            <p class="text-muted">&copy; Copyright. All rights reserved. Made with extreme care by <a href="http://www.neilromana.com" target="_blank"> Neil Romana.</a></p>
+          </div>
+        </div>
+      </div>
+    </footer>
+	)
+};
+
+export default Footer;
