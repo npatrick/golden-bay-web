80afa4addbbb0302d268bc51340cc857b8597281
Nicko Romana <wazzupnickz@gmail.com>
2017-04-13 16:36:50 -0700

    add about component

---
+++ src/client/components/about.js
@@ -0,0 +1,43 @@
+import React from 'react';
+
+const About = () => {
+	return (
+		<section id="about">
+      <div className="container">
+        <div className="row">
+          <div className="col-xs-12">
+            <p className="fancy"><span>About Us</span></p>
+          </div>
+          <div className="article col-xs-12 col-sm-4">
+            <i className="ion-clock fa-2x orange"></i>
+            <h3>History</h3>
+            <p>We started in 2007 wanting to build something new for online content. Our founders, James T. Kirk and Caption Nemo, met in 7th grade but didn't start working on Sunshine until a few years later at UC Davis (right before dropping out).</p>
+          </div>
+          <div className="article col-xs-12 col-sm-4">
+            <i className="ion-compass fa-2x orange"></i>
+            <h3>Philosophy</h3>
+            <p>We believe that how you see something is just as important as what you see. You don't need to trade in your identity for the ability to view things. The freedom to see how you want is what lets you be truly authentic.</p>
+          </div>
+          <div className="article col-xs-12 col-sm-4">
+            <i className="ion-pinpoint fa-2x orange"></i>
+            <h3>Motivation</h3>
+            <p>Sunshine is made for designers by designers. Everyone is a designer about something, whether it's music, food, or science fiction. Sometimes you just need the right space to explore it. Whatever you're into, we help you get into it.</p>
+          </div>
+        </div>
+        <div id="facts" className="carousel slide hidden-xs">
+
+          <div className="carousel-inner">
+            <div className="active item"><h3><strong>Founded</strong> in 2007</h3></div>
+            <div className="item"><h3><strong>1000 websites</strong> designed</h3></div>
+            <div className="item"><h3>Received the <strong>Alpha award</strong> in 2008</h3></div>
+            <div className="item"><h3>Won the <strong>Best website</strong> award in 2010</h3></div>
+            <div className="item"><h3><strong>Pure Magic UI kit</strong> released in 2012</h3></div>
+          </div>
+
+        </div>
+      </div>
+    </section>
+	);
+};
+
+export default About;
--- src/client/components/app.js
+++ src/client/components/app.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Navigation from './navigation';
 import Home from './home';
+import About from './about';
 import Services from './services';
 import Work	from './work';
 import Client from './client';
@@ -14,6 +15,7 @@ export default class App extends Component {
 			<div>
 				<Navigation />
 				<Home />
+				<About />
 				<Services />
 				<Work />
 				<Client />
