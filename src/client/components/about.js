6af03ef1ca89c1071e4d56a4e128372d8c5678c8
Nicko Romana <wazzupnickz@gmail.com>
2017-04-25 22:51:44 -0700

    add carousel text on about us

--- src/client/components/about.js
+++ src/client/components/about.js
@@ -10,13 +10,13 @@ const About = () => {
           </div>
           <div className="article col-xs-12 col-sm-4">
             <i className="ion-clock fa-2x orange"></i>
-            <h3>History</h3>
-            <p>We started in 2007 wanting to build something new for online content. Our founders, James T. Kirk and Caption Nemo, met in 7th grade but didn't start working on Sunshine until a few years later at UC Davis (right before dropping out).</p>
+            <h3>Assessment</h3>
+            <p>We provide complimentary assessment by qualified staff and family conferences to ensure the most appropriate and cost effective plan. Interview process is conducted to attain in-depth assessment of the client's lifestyle, medical condition, and living arrangements where we offer customized options to fit your needs.</p>
           </div>
           <div className="article col-xs-12 col-sm-4">
             <i className="ion-compass fa-2x orange"></i>
             <h3>Philosophy</h3>
-            <p>We believe that how you see something is just as important as what you see. You don't need to trade in your identity for the ability to view things. The freedom to see how you want is what lets you be truly authentic.</p>
+            <p>We believe that everyone deserves a highest level quality of life that is achievable. To preserve independence, dignity for aging loved ones who wish to live at home, while providing peace of mind to their familes through the dedicated services of competent and caring professional caregivers.</p>
           </div>
           <div className="article col-xs-12 col-sm-4">
             <i className="ion-pinpoint fa-2x orange"></i>
@@ -27,11 +27,11 @@ const About = () => {
         <div id="facts" className="carousel slide hidden-xs">
 
           <div className="carousel-inner">
-            <div className="active item"><h3><strong>Founded</strong> in 2007</h3></div>
-            <div className="item"><h3><strong>1000 websites</strong> designed</h3></div>
-            <div className="item"><h3>Received the <strong>Alpha award</strong> in 2008</h3></div>
-            <div className="item"><h3>Won the <strong>Best website</strong> award in 2010</h3></div>
-            <div className="item"><h3><strong>Pure Magic UI kit</strong> released in 2012</h3></div>
+            <div className="active item"><h3><strong>Founded</strong> in 2010</h3></div>
+            <div className="item"><h3><strong>Experienced</strong> In Home Health Aides</h3></div>
+            <div className="item"><h3><strong>Certified</strong> Nursing Assistants</h3></div>
+            <div className="item"><h3><strong>Healthcare</strong> Professionals</h3></div>
+            <div className="item"><h3><strong>Care Providers</strong> and <strong>Relief</strong> Available <strong>24 Hours a Day</strong></h3></div>
           </div>
 
         </div>
