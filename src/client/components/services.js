44c102353fe7ad542d75286ecbda87d465f10aff
Nicko Romana <wazzupnickz@gmail.com>
2017-05-17 18:39:11 -0700

    fix service rate position

--- src/client/components/services.js
+++ src/client/components/services.js
@@ -55,17 +55,21 @@ const Services = () => {
               <li>All employees are hired, managed, and paid by Golden Bay</li>
             </ul>
           </div>
-          <div className="desc col-xs-12 col-sm-6 col-sm-offset-3">
+          <div id="prices" className="desc col-xs-12 col-sm-6 col-sm-offset-3">
             <i className="ion-ios7-people fa-2x orange"></i>
             <h2>In Home Care Rates</h2>
-            <ul className="list-unstyled">
-              <li><h3>Hourly (Live-Out)</h3></li>
-              <li><h3>$24.00 and Up Per Hour</h3></li>
-            </ul>
-            <ul className="list-unstyled">
-              <li><h3>Residing (Live-In)</h3></li>
-              <li><h3>$235.00  and Up Per Day</h3></li>
-            </ul>
+            <div className="article col-xs-12 col-sm-6">
+              <ul className="list-unstyled">
+                <li><h3>Hourly (Live-Out)</h3></li>
+                <li><h3>$24.00 and Up Per Hour</h3></li>
+              </ul>
+            </div>
+            <div className="article col-xs-12 col-sm-6">
+              <ul className="list-unstyled">
+                <li><h3>Residing (Live-In)</h3></li>
+                <li><h3>$235.00  and Up Per Day</h3></li>
+              </ul>
+            </div>
           </div>
         </div>
       </div>
