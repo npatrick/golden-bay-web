7d1806f4ac8e67626287ced601e17378a2fd79bb
Nicko Romana <wazzupnickz@gmail.com>
2017-05-31 20:52:32 -0700

    fix unnecessary api

--- src/server/server.js
+++ src/server/server.js
@@ -13,7 +13,7 @@ app.use(bodyParser.json());
 
 app.use(express.static(path.join(__dirname, '../../public/')));
 
-app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../public/')));
+// app.get('/', (req, res) => res.sendFile(path.join(__dirname, '../../public/')));
 
 app.post('/send_message', (req, res) => {
 	console.log('POST INCOMING: ', req.body);
