3c853b7eaf524358720b0b2b883a59797bf39296
Nicko Romana <wazzupnickz@gmail.com>
2017-05-31 23:23:42 -0700

    fix text info

--- src/client/components/comingSoon.react.jsx
+++ src/client/components/comingSoon.react.jsx
@@ -6,7 +6,7 @@ const ComingSoon = () => {
 			<div className="row">
 				<div className="desc col-xs-12 col-sm-12 intro">
 					<h3>Thank you for visiting Golden Bay Health & Staffing Services, LLC</h3>
-					<h4>Unfortunately, our site is still in the works!</h4>
+					<h4>Our site is still in the works!</h4>
 					<h4>Please come back later or contact <strong>(310) 328-4541</strong> for more info.</h4>
 				</div>
 			</div>
