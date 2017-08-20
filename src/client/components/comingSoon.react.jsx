7b4ffd12938945a60120b5a8d7696655c4ecd74b
MADEL ENCARNACION <madelencarnacion@MADELs-MacBook-Pro.local>
2017-08-19 22:18:29 -0700

    fix coming soon container spacing

--- src/client/components/comingSoon.react.jsx
+++ src/client/components/comingSoon.react.jsx
@@ -1,14 +1,18 @@
 import React from 'react';
 
 const ComingSoon = () => {
+
+	const comingSoonStyle = {
+		'marginTop': '10%',
+		'opacity': '0.75'
+	};
+
 	return (
 		<div className="container" id="coming-soon">
-			<div className="row">
-				<div className="desc col-xs-12 col-sm-12 intro">
-					<h3>Thank you for visiting Golden Bay Health & Staffing Services, LLC</h3>
-					<h4>Our site is still in the works!</h4>
-					<h4>Please come back later or contact <strong>(310) 328-4541</strong> for more info.</h4>
-				</div>
+			<div className="jumbotron" style={comingSoonStyle}>
+				<h3>Thank you for visiting Golden Bay Health & Staffing Services, LLC</h3>
+				<h4>Our site is still in the works!</h4>
+				<h4>Please come back later or contact <strong>(310) 328-4541</strong> for more info.</h4>
 			</div>
 		</div>
 
